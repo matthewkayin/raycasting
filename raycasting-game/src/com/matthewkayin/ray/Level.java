bf25c97b0b79dcb1df0c7651d92159e171c18ff0
Matt Madden <matthewkayin@gmail.com>
2019-01-01 15:53:53 -0600

    Scaled up resolution, added mouse based rotation, cleaned up code from prototype version
    Signed-off-by: Matt Madden <matthewkayin@gmail.com>

---
+++ raycasting-game/src/com/matthewkayin/ray/Level.java
@@ -0,0 +1,416 @@
+package com.matthewkayin.ray;
+
+import java.lang.Math;
+
+public class Level{
+
+    private double position[];
+    private double direction[];
+    private int map[][] =  {{1, 1, 1, 1, 1, 1},
+                            {1, 0, 0, 0, 0, 1},
+                            {1, 0, 0, 0, 0, 1},
+                            {1, 0, 1, 1, 0, 1},
+                            {1, 0, 1, 1, 0, 1},
+                            {1, 1, 1, 1, 1, 1}};
+    private final int RANGE = 5;
+    private final double SPEED = 0.01;
+    private final double ROT_SPEED = 2;
+
+    public Level(){
+
+        position = new double[]{1.5, 1.5};
+        direction = new double[]{RANGE, 0};
+    }
+
+    void movePlayer(double delta, double angle){
+
+        double step[] = rotate(direction, angle);
+        step[0] = step[0] * delta * SPEED;
+        step[1] = step[1] * delta * SPEED;
+        position[0] += step[0];
+        position[1] += step[1];
+
+        //Don't perform movement if movement will result in collision with wall
+        if(map[(int)position[0]][(int)position[1]] != 0){
+
+            position[0] -= step[0];
+            position[1] -= step[1];
+        }
+    }
+
+    void rotatePlayer(double delta, double input){
+
+        direction = rotate(direction, delta * input * ROT_SPEED * -1);
+    }
+
+    public double raycast(double angle){
+
+        double dir[] = rotate(direction, angle);
+        double xdist = dir[0];
+        double ydist = dir[1];
+        double distance = 0;
+        int xdir = 0;
+        int ydir = 0;
+
+        if(xdist > 0){
+
+            xdir = 1;
+
+        }else if(xdist < 0){
+
+            xdir = -1;
+        }
+        if(ydist > 0){
+
+            ydir = 1;
+
+        }else if(ydist < 0) {
+
+            ydir = -1;
+        }
+
+        double point[] = {position[0], position[1]};
+        boolean hit = false;
+
+        while(!hit && getMagnitude(new double[]{point[0] - position[0], point[1] - position[1]}) <= RANGE){
+
+            if(xdir == 0){
+
+                if((int)(point[1]) != point[1]){
+
+                    if(ydir > 0){
+
+                        point[1] = (int)(point[1] + 1);
+
+                    }else{
+
+                        point[1] = (int)(point[1]);
+                    }
+
+                }else{
+
+                    point[1] += ydir;
+                }
+
+                distance = getMagnitude(new double[]{point[0] - position[0], point[1] - position[1]});
+
+            }else if(ydir == 0){
+
+                if((int)(point[0]) != point[0]){
+
+                    if(xdir > 0){
+
+                        point[0] = (int)(point[0] + 1);
+
+                    }else{
+
+                        point[0] = (int)(point[0]);
+                    }
+
+                }else{
+
+                    point[0] += xdir;
+                }
+
+                distance = getMagnitude(new double[]{point[0] - position[0], point[1] - position[1]});
+
+            }else{
+
+                //first try going x
+                double x = point[0];
+                double y = point[1];
+                double x2 = point[0];
+                double y2 = point[1];
+                double dist = 0;
+                if((int)(x) != x){
+
+                    if(xdir > 0){
+
+                        x = (int)(x + 1);
+
+                    }else{
+
+                        x = (int)(x);
+                    }
+
+                }else{
+
+                    x += xdir;
+                }
+                y += (x - point[0]) / (xdist / ydist);
+                dist = getMagnitude(new double[]{x - position[0], y - position[1]});
+                if((int)(y2) != y2){
+
+                    if(ydir > 0){
+
+                        y2 = (int)(y2 + 1);
+
+                    }else{
+
+                        y2 = (int)(y2);
+                    }
+
+                }else{
+
+                    y2 += ydir;
+                }
+                x2 += (y2 - point[1]) * (xdist / ydist);
+                distance = getMagnitude(new double[]{x2 - position[0], y2 - position[1]});
+                if(dist <= distance){
+
+                    distance = dist;
+                    point[0] = x;
+                    point[1] = y;
+
+                }else{
+
+                    point[0] = x2;
+                    point[1] = y2;
+                }
+            }
+
+            hit = onWall(point[0], point[1], xdir, ydir);
+        }
+
+        if(!hit || getMagnitude(new double[]{point[0] - position[0], point[1] - position[1]}) > RANGE){
+
+            return -1;
+        }
+
+        return distance;
+    }
+
+    private double[] getRaycastPoint(double dir[]){
+
+        double xdist = dir[0];
+        double ydist = dir[1];
+        int xdir = 0;
+        int ydir = 0;
+
+        if(xdist > 0){
+
+            xdir = 1;
+
+        }else if(xdist < 0){
+
+            xdir = -1;
+        }
+        if(ydist > 0){
+
+            ydir = 1;
+
+        }else if(ydist < 0) {
+
+            ydir = -1;
+        }
+
+        double point[] = {position[0], position[1]};
+        boolean hit = false;
+
+        while(!hit && getMagnitude(new double[]{point[0] - position[0], point[1] - position[1]}) <= RANGE){
+
+            if(xdir == 0){
+
+                if((int)(point[1]) != point[1]){
+
+                    if(ydir > 0){
+
+                        point[1] = (int)(point[1] + 1);
+
+                    }else{
+
+                        point[1] = (int)(point[1]);
+                    }
+
+                }else{
+
+                    point[1] += ydir;
+                }
+
+            }else if(ydir == 0){
+
+                if((int)(point[0]) != point[0]){
+
+                    if(xdir > 0){
+
+                        point[0] = (int)(point[0] + 1);
+
+                    }else{
+
+                        point[0] = (int)(point[0]);
+                    }
+
+                }else{
+
+                    point[0] += xdir;
+                }
+
+            }else{
+
+                //first try going x
+                double x = point[0];
+                double y = point[1];
+                double x2 = point[0];
+                double y2 = point[1];
+                double dist = 0;
+                if((int)(x) != x){
+
+                    if(xdir > 0){
+
+                        x = (int)(x + 1);
+
+                    }else{
+
+                        x = (int)(x);
+                    }
+
+                }else{
+
+                    x += xdir;
+                }
+                y += (x - point[0]) / (xdist / ydist);
+                dist = getMagnitude(new double[]{x - position[0], y - position[1]});
+                if((int)(y2) != y2){
+
+                    if(ydir > 0){
+
+                        y2 = (int)(y2 + 1);
+
+                    }else{
+
+                        y2 = (int)(y2);
+                    }
+
+                }else{
+
+                    y2 += ydir;
+                }
+                x2 += (y2 - point[1]) * (xdist / ydist);
+                if(dist <= getMagnitude(new double[]{x2 - position[0], y2 - position[1]})){
+
+                    point[0] = x;
+                    point[1] = y;
+
+                }else{
+
+                    point[0] = x2;
+                    point[1] = y2;
+                }
+            }
+
+            hit = onWall(point[0], point[1], xdir, ydir);
+        }
+
+        if(!hit || getMagnitude(new double[]{point[0] - position[0], point[1] - position[1]}) > RANGE){
+
+            point[0] = -1;
+            point[1] = -1;
+        }
+
+        return point;
+    }
+
+    private boolean onWall(double x, double y, int xdir, int ydir){
+
+        int ix = (int)x;
+        int iy = (int)y;
+
+        if(x == ix && y == iy){
+
+            if(xdir == 0){
+
+                if(ydir < 0){
+
+                    return checkPoint(ix - 1, iy - 1) || checkPoint(ix, iy - 1);
+
+                }else{
+
+                    return checkPoint(ix, iy) || checkPoint(ix - 1, iy);
+                }
+
+            }else if(ydir == 0){
+
+                if(xdir < 0){
+
+                    return checkPoint(ix - 1, iy) || checkPoint(ix - 1, iy - 1);
+
+                }else{
+
+                    return checkPoint(ix, iy) || checkPoint(ix, iy - 1);
+                }
+
+            }else if(xdir < 0 && ydir < 0){
+
+                return checkPoint(ix - 1, iy - 1);
+
+            }else if(xdir < 0 && ydir > 0){
+
+                return checkPoint(ix - 1, iy);
+
+            }else if(xdir > 0 && ydir < 0){
+
+                return checkPoint(ix, iy - 1);
+
+            }else if(xdir > 0 && ydir > 0){
+
+                return checkPoint(ix, iy);
+
+            }else{
+
+                return false;
+            }
+
+        }else if(iy != y && ix == x){
+
+            if(xdir > 0){
+
+                return checkPoint(ix, iy);
+
+            }else{
+
+                return checkPoint(ix - 1, iy);
+            }
+
+        }else if(iy == y && ix != x){
+
+            if(ydir > 0){
+
+                return checkPoint(ix, iy);
+
+            }else{
+
+                return checkPoint(ix, iy - 1);
+            }
+
+        }else{
+
+            return false;
+        }
+    }
+
+    private boolean checkPoint(int x, int y){
+
+        return (x >= 0 && x <= map[0].length - 1 && y >= 0 && y <= map.length - 1 && map[y][x] != 0);
+    }
+
+    private double getMagnitude(double point[]){
+
+        return Math.sqrt( (point[0] * point[0]) + (point[1] * point[1]) );
+    }
+
+    private double[] rotate(double other[], double angle){
+
+        double rm[][] = getRotationMatrix(angle);
+        return new double[]{rm[0][0] * other[0] + rm[1][0] * other[1], rm[0][1] * other[0] + rm[1][1] * other[1]};
+    }
+
+    private double[][] getRotationMatrix(double angle){
+
+        double rads = Math.toRadians(angle);
+        double a = Math.cos(rads);
+        double b = Math.sin(rads);
+        double c = -1 * b;
+
+        return new double[][]{{a, c}, {b, a}};
+    }
+}
--- raycasting-game/src/com/matthewkayin/ray/Main.java
+++ raycasting-game/src/com/matthewkayin/ray/Main.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
 import java.lang.Math;
 
 public class Main extends JPanel{
@@ -17,8 +18,8 @@ public class Main extends JPanel{
     private int frames;
     private int fps;
 
-    private final int SCREEN_WIDTH = 320;
-    private final int SCREEN_HEIGHT = 200;
+    private final int SCREEN_WIDTH = 1280;
+    private final int SCREEN_HEIGHT = 720;
 
     private boolean keydown[] = {false, false, false, false, false, false};
     private final int W = 0;
@@ -27,18 +28,14 @@ public class Main extends JPanel{
     private final int D = 3;
     private final int Q = 4;
     private final int E = 5;
+    private int mousex = 0;
+    private int mousey = 0;
+    private int oldmousex = 0;
+    private int oldmousey = 0;
+    private boolean lockMouse = false;
 
-    private double playerx = 1.5;
-    private double playery = 1.5;
-    private double directionx = 5;
-    private double directiony = 0;
-    private boolean showMap = false;
-    private int map[][] =   {{1, 1, 1, 1, 1, 1},
-                             {1, 0, 0, 0, 0, 1},
-                             {1, 0, 0, 0, 0, 1},
-                             {1, 0, 1, 1, 0, 1},
-                             {1, 0, 1, 1, 0, 1},
-                             {1, 1, 1, 1, 1, 1}};
+    private Level level;
+    private Robot robot;
 
     public Main(){
 
@@ -47,6 +44,35 @@ public class Main extends JPanel{
         setFocusable(true);
         requestFocus();
         setBackground(Color.black);
+
+        addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mousePressed(MouseEvent e){
+
+                if(!lockMouse){
+
+                    lockMouse = true;
+                }
+            }
+        });
+
+        addMouseMotionListener(new MouseMotionAdapter(){
+
+            @Override
+            public void mouseMoved(MouseEvent e){
+
+                mousex = e.getX();
+                mousey = e.getY();
+            }
+
+            @Override
+            public void mouseDragged(MouseEvent e){
+
+                mousex = e.getX();
+                mousey = e.getY();
+            }
+        });
 
         addKeyListener(new KeyAdapter() {
 
@@ -59,6 +85,10 @@ public class Main extends JPanel{
                 if(keycode == KeyEvent.VK_ESCAPE){
 
                     running = false;
+
+                }else if(keycode == KeyEvent.VK_F2){
+
+                    lockMouse = !lockMouse;
                 }
 
                 switch(keycode){
@@ -112,6 +142,17 @@ public class Main extends JPanel{
             }
         });
 
+        level = new Level();
+
+        try{
+
+            robot = new Robot();
+
+        }catch(Exception e){
+
+            e.printStackTrace();
+        }
+
         running = false;
     }
 
@@ -161,17 +202,13 @@ public class Main extends JPanel{
             return;
         }
 
-        if(keydown[Q]){
+        if(lockMouse){
 
-            double newDir[] = rotate(directionx, directiony, 1 * delta);
-            directionx = newDir[0];
-            directiony = newDir[1];
+            int xdiff = mousex - (SCREEN_WIDTH / 2);
+            int ydiff = mousey - (SCREEN_HEIGHT / 2);
 
-        }else if(keydown[E]){
-
-            double newDir[] = rotate(directionx, directiony, -1 * delta);
-            directionx = newDir[0];
-            directiony = newDir[1];
+            robot.mouseMove(this.getLocationOnScreen().x + (SCREEN_WIDTH / 2), this.getLocationOnScreen().y + (SCREEN_HEIGHT / 2));
+            level.rotatePlayer(delta, (double)xdiff / (SCREEN_WIDTH / 17));
         }
 
         boolean shouldmove = true;
@@ -218,16 +255,7 @@ public class Main extends JPanel{
 
         if(shouldmove){
 
-            double SPEED = 0.01;
-            double newDir[] = rotate(directionx * SPEED * delta, directiony * SPEED * delta, moveangle);
-            playerx += newDir[0];
-            playery += newDir[1];
-
-            if(map[(int)playery][(int)playerx] != 0){
-
-                playerx -= newDir[0];
-                playery -= newDir[1];
-            }
+            level.movePlayer(delta, moveangle);
         }
     }
 
@@ -237,315 +265,47 @@ public class Main extends JPanel{
         Graphics2D g2d = (Graphics2D)g;
 
         g2d.setColor(Color.red);
-        if(showMap){
-
-            for(int i = 0; i < map.length; i++){
-
-                for(int j = 0; j < map[i].length; j++){
-
-                    if(map[i][j] == 1){
-
-                        g2d.fillRect(j * 32, i * 32, 32, 32);
-
-                    }else{
-
-                        g2d.drawRect(j * 32, i * 32, 32, 32);
-                    }
-                }
-            }
-
-            g2d.setColor(Color.green);
-            g2d.fillRect((int)(playerx * 32) - 2, (int)(playery * 32) - 2, 4, 4);
-            g2d.setColor(Color.yellow);
-        }
-
-        for(int i = 0; i < SCREEN_WIDTH; i++){
-
-            double angle = -30 + (0.1875 * i);
-            double newDir[] = rotate(directionx, directiony, angle);
-            double point[] = raycast(new double[]{playerx, playery}, newDir);
-            if(showMap){
-
-                if(i == 0 || i == SCREEN_WIDTH - 1){
-
-                    g2d.drawLine((int)(playerx * 32), (int)(playery * 32), (int)((playerx + newDir[0]) * 32), (int)((playery + newDir[1]) * 32));
-                }
-                if(point[0] != -1){
-
-                    g2d.fillRect((int)(point[0] * 32) - 2, (int)(point[1] * 32) - 2, 4, 4);
-                }
-
-            }else{
-
-                if(point[0] != -1){
-
-                    double distance = (getMagnitude(new double[]{point[0] - playerx, point[1] - playery})) * Math.cos(Math.toRadians(angle));
-                    int height = (int)((64 * 3) / distance);
-                    int start = (SCREEN_HEIGHT - height) / 2;
-                    int x = (SCREEN_WIDTH - i - 1);
-                    g2d.drawLine(x, start, x, start + height);
-                }
-            }
-        }
+        renderLevel(g2d);
 
         Toolkit.getDefaultToolkit().sync();
         g2d.dispose();
     }
 
-    private double[] rotate(double x, double y, double angle){
+    private void renderLevel(Graphics2D g2d){
 
-        double rVal[] = {x, y};
-        double rm[][] = getRotationMatrix(angle);
-        rVal[0] = (rm[0][0] * x + rm[1][0] * y);
-        rVal[1] = (rm[0][1] * x + rm[1][1] * y);
+        int width = SCREEN_WIDTH / 320;
+        double anglestep = 60 / (SCREEN_WIDTH * (1 / (double)width));
+        for(int i = 0; i < SCREEN_WIDTH; i++){
 
-        return rVal;
-    }
+            double angle = -30 + (anglestep * i);
+            double distance = level.raycast(angle);
+            if(distance != -1){
 
-    private double[][] getRotationMatrix(double angle){
-
-        double rads = Math.toRadians(angle);
-        double a = (Math.cos(rads));
-        double b = Math.sin(rads);
-        b *= -1;
-        double c = Math.sin(rads);
-
-        double rVal[][] = {{a, b}, {c, a}};
-
-        return rVal;
-    }
-
-    private double[] raycast(double position[], double direction[]){
-
-        double xdist = direction[0];
-        double ydist = direction[1];
-        final double RANGE = getMagnitude(direction);
-        int xdir = 0;
-        int ydir = 0;
-
-        if(xdist > 0){
-
-            xdir = 1;
-
-        }else if(xdist < 0){
-
-            xdir = -1;
-        }
-        if(ydist > 0){
-
-            ydir = 1;
-
-        }else if(ydist < 0) {
-
-            ydir = -1;
-        }
-
-        double point[] = {position[0], position[1]};
-        boolean hit = false;
-
-        while(!hit && getMagnitude(new double[]{point[0] - position[0], point[1] - position[1]}) <= RANGE){
-
-            if(xdir == 0){
-
-                if((int)(point[1]) != point[1]){
-
-                    if(ydir > 0){
-
-                        point[1] = (int)(point[1] + 1);
-
-                    }else{
-
-                        point[1] = (int)(point[1]);
-                    }
-
-                }else{
-
-                    point[1] += ydir;
-                }
-
-            }else if(ydir == 0){
-
-                if((int)(point[0]) != point[0]){
-
-                    if(xdir > 0){
-
-                        point[0] = (int)(point[0] + 1);
-
-                    }else{
-
-                        point[0] = (int)(point[0]);
-                    }
-
-                }else{
-
-                    point[0] += xdir;
-                }
-
-            }else{
-
-                //first try going x
-                double x = point[0];
-                double y = point[1];
-                double x2 = point[0];
-                double y2 = point[1];
-                double dist = 0;
-                if((int)(x) != x){
-
-                    if(xdir > 0){
-
-                        x = (int)(x + 1);
-
-                    }else{
-
-                        x = (int)(x);
-                    }
-
-                }else{
-
-                    x += xdir;
-                }
-                y += (x - point[0]) / (xdist / ydist);
-                dist = getMagnitude(new double[]{x - position[0], y - position[1]});
-                if((int)(y2) != y2){
-
-                    if(ydir > 0){
-
-                        y2 = (int)(y2 + 1);
-
-                    }else{
-
-                        y2 = (int)(y2);
-                    }
-
-                }else{
-
-                    y2 += ydir;
-                }
-                x2 += (y2 - point[1]) * (xdist / ydist);
-                if(dist <= getMagnitude(new double[]{x2 - position[0], y2 - position[1]})){
-
-                    point[0] = x;
-                    point[1] = y;
-
-                }else{
-
-                    point[0] = x2;
-                    point[1] = y2;
-                }
+                distance = distance * Math.cos(Math.toRadians(angle));
+                int height = (int)(SCREEN_HEIGHT / distance);
+                int y = (SCREEN_HEIGHT - height) / 2;
+                int x = (SCREEN_WIDTH - ((i * width) + 1));
+                g2d.fillRect(x, y, width, height);
             }
-
-            hit = onWall(point[0], point[1], xdir, ydir);
         }
-
-        if(!hit || getMagnitude(new double[]{point[0] - position[0], point[1] - position[1]}) > RANGE){
-
-            point[0] = -1;
-            point[1] = -1;
-        }
-
-        return point;
-    }
-
-    private boolean onWall(double x, double y, int xdir, int ydir){
-
-        int ix = (int)x;
-        int iy = (int)y;
-
-        if(x == ix && y == iy){
-
-            if(xdir == 0){
-
-                if(ydir < 0){
-
-                    return checkPoint(ix - 1, iy - 1) || checkPoint(ix, iy - 1);
-
-                }else{
-
-                    return checkPoint(ix, iy) || checkPoint(ix - 1, iy);
-                }
-
-            }else if(ydir == 0){
-
-                if(xdir < 0){
-
-                    return checkPoint(ix - 1, iy) || checkPoint(ix - 1, iy - 1);
-
-                }else{
-
-                    return checkPoint(ix, iy) || checkPoint(ix, iy - 1);
-                }
-
-            }else if(xdir < 0 && ydir < 0){
-
-                return checkPoint(ix - 1, iy - 1);
-
-            }else if(xdir < 0 && ydir > 0){
-
-                return checkPoint(ix - 1, iy);
-
-            }else if(xdir > 0 && ydir < 0){
-
-                return checkPoint(ix, iy - 1);
-
-            }else if(xdir > 0 && ydir > 0){
-
-                return checkPoint(ix, iy);
-
-            }else{
-
-                return false;
-            }
-
-        }else if(iy != y && ix == x){
-
-            if(xdir > 0){
-
-                return checkPoint(ix, iy);
-
-            }else{
-
-                return checkPoint(ix - 1, iy);
-            }
-
-        }else if(iy == y && ix != x){
-
-            if(ydir > 0){
-
-                return checkPoint(ix, iy);
-
-            }else{
-
-                return checkPoint(ix, iy - 1);
-            }
-
-        }else{
-
-            return false;
-        }
-    }
-
-    private boolean checkPoint(int x, int y){
-
-        return (x >= 0 && x <= map[0].length - 1 && y >= 0 && y <= map.length - 1 && map[y][x] != 0);
-    }
-
-    private double getMagnitude(double point[]){
-
-        return Math.sqrt( (point[0] * point[0]) + (point[1] * point[1]) );
     }
 
     public static void main(String[] args){
 
         JFrame window = new JFrame("Raycaster");
-        window.setSize(320, 200);
+        window.setSize(1280, 780);
         window.setResizable(false);
-        window.setLocationRelativeTo(null);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        BufferedImage cursorImg = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+        Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(cursorImg, new Point(0, 0), "blank cursor");
+        window.getContentPane().setCursor(blankCursor);
 
         Main game = new Main();
         window.add(game);
         window.pack();
 
+        window.setLocationRelativeTo(null);
         window.setVisible(true);
 
         game.run();
